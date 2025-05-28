7e3837ef9992e93072452b1b8f8a6fecf9ac66d3
Panchu11 <panchananefs@gmail.com>
2025-05-28 23:54:49 +0530

    Fix Tailwind CSS configuration

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,15 @@
+import tailwindcss from '@tailwindcss/postcss';
+
 const config = {
-  plugins: ["@tailwindcss/postcss"],
+  plugins: [
+    tailwindcss({
+      content: [
+        './src/**/*.{js,ts,jsx,tsx,css}',
+        './src/app/**/*.{js,ts,jsx,tsx,css}',
+        './src/components/**/*.{js,ts,jsx,tsx,css}'
+      ]
+    })
+  ],
 };
 
 export default config;
